59d8b6be9c5705ef7afe4c1750ebd83582742a60
Y-Lammers <youri_lammers_88@hotmail.com>
2015-11-25 12:28:09 +0000

    Comparing BLAST files without abundance data now works with krona.

--- Wrappers/Compare_BLAST.sh
+++ Wrappers/Compare_BLAST.sh
@@ -15,7 +15,6 @@ then
 	do
 		IFS=$' \t\n'
 		output=$(echo "$tsv" | sed 's/.filtered.*//')
-		echo "$tsv" "$output"
 		# unzip the blast to the tsv file, when unzipping the BLAST
 		# data is automatically filtered to only use the first BLAST
 		# hit per cluster
@@ -27,7 +26,7 @@ then
 		if [ "$2" != "-A" ]
 		then
 			# convert the tsv files and set cluster size to 1
-			cut -f11 "$output" | sed -e 's/ \/ /\t/g' | sed -e "s/\t/\t${output%.*}\t1\t/1" >> "$temp"
+			cut -f11 "$output" | sed -e 's/ \/ /\t/g' | sed -e "s/^/${output%.*}\t1\t/1" >> "$temp"
 		else
 			# convert the tsv file to the krona format while
 			# maintaining the cluster size
@@ -35,12 +34,8 @@ then
 		fi
 	done
 
-	echo $flist
-
 	# Run the Compare blast script
 	Compare_BLAST "$6" "$2" $flist
-
-	head "$6"
 
 	# create the krona html file with or without abundance based on the user setting
 	if [ "$2" != "-A" ]
@@ -56,15 +51,15 @@ then
 		# create an ordered or unordered heatmap based on user settings
 		if [ "$5" == "order" ]
 		then
-			heatmap_BLAST.R "$6" "${3}.png" "$rankname" "$rankpos" "order" > /dev/null 2>&1
+			Heatmap_BLAST "$6" "${3}.png" "$rankname" "$rankpos" "order" > /dev/null 2>&1
 		else
-			heatmap_BLAST.R "$6" "${3}.png" "$rankname" "$rankpos" "unorder" > /dev/null 2>&1
+			Heatmap_BLAST "$6" "${3}.png" "$rankname" "$rankpos" "unorder" > /dev/null 2>&1
 		fi
 		mv "${3}.png" "$3"
 	fi
 
 	# remove the temp file
-	rm "$temp" $flist
+	rm "$temp" $(printf $flist | sort -u)
 
 
 # if a non-zipped BLAST file is provided
@@ -95,13 +90,12 @@ else
 		if [ "$2" != "-A" ]
 		then
 			# convert the BLAST file without the cluster sizes
-			cut -f11 "$fblast" | sed -e 's/ \/ /\t/g' | sed -e "s/\t/\t$name\t/1" >> "$temp"
+			cut -f11 "$fblast" | sed -e 's/ \/ /\t/g' | sed -e "s/^/$name\t/1" >> "$temp"
 		else
 			# convert the BLAST file to the input format required by krona
 			# while maintaining the cluster sizes
 			cut -f2,12 "$fblast" | sed -e 's/ \/ /\t/g' | sed -e "s/\t/\t$name\t/1" >> "$temp"
 		fi
-		echo $flist
 	done
 
 	# Run the Compare blast script
@@ -121,13 +115,13 @@ else
 		# generate an ordered or unordered heatmap (based on user setting)
 		if [ "$5" == "order" ]
 		then
-			heatmap_BLAST.R "$6" "${3}.png" "$rankname" "$rankpos" "order" > /dev/null 2>&1
+			Heatmap_BLAST "$6" "${3}.png" "$rankname" "$rankpos" "order" > /dev/null 2>&1
 		else
-			heatmap_BLAST.R "$6" "${3}.png" "$rankname" "$rankpos" "unorder" > /dev/null 2>&1
+			Heatmap_BLAST "$6" "${3}.png" "$rankname" "$rankpos" "unorder" > /dev/null 2>&1
 		fi
 		mv "${3}.png" "$3"
 	fi
 
 	# remove the temp file
-	rm "$temp" $flist
+	rm "$temp" $(printf $flist | sort -u)
 fi

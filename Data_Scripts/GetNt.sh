576338eef8a14184374af51d0eea9aefd95da342
Y-Lammers <youri_lammers_88@hotmail.com>
2016-02-09 09:16:04 +0100

    Removed the filtered BLAST option from the GetNt.sh script, since it is no longer implemented in the blast wrapper.

--- Data_Scripts/GetNt.sh
+++ Data_Scripts/GetNt.sh
@@ -6,10 +6,10 @@ update_blastdb --passive nt
 
 for i in *.gz; do tar -xzvf $i; rm $i; done
 
-/home/galaxy/Tools/GetUpdate/GetJunkGI.py > junk.gi.txt
+#/home/galaxy/Tools/GetUpdate/GetJunkGI.py > junk.gi.txt
 
-blastdbcmd -db nt -entry all -outfmt %g > allseq.gi.txt
+#blastdbcmd -db nt -entry all -outfmt %g > allseq.gi.txt
 
-comm -23 <(sort allseq.gi.txt) <(sort junk.gi.txt) > clean.gi.txt
+#comm -23 <(sort allseq.gi.txt) <(sort junk.gi.txt) > clean.gi.txt
 
-blastdb_aliastool -gilist clean.gi.txt -dbtype nucl -db nt -out nt_clean -title nt_clean
+#blastdb_aliastool -gilist clean.gi.txt -dbtype nucl -db nt -out nt_clean -title nt_clean
